3f188171df4411245035352fa8213e39073ab5b8
licup <41974174+licup@users.noreply.github.com>
2019-05-09 09:42:49 -0700

    preset items included

--- implementation/Inventory.cpp
+++ implementation/Inventory.cpp
@@ -1,17 +1,20 @@
 //
 //  Inventory.cpp
 //  PokemonGame
-//
-//  Created by Jr on 4/18/19.
-//  Copyright © 2019 Edgar Licup. All rights reserved.
-//
 
 #include "Inventory.hpp"
+#include <string>
+
 Inventory::Inventory() {
-    quantity = 0;
+    item[0] = Items("Pokéball", "This lets you have a chance at catching a pokémon!", false);
+    item[1] = Items("Potion", "This heals your pokémon by 20hp", true, 20);
+    item[2] = Items("Great Potion", "This heals your pokémon by 40hp", true, 40);
+    item[3] = Items("Revive", "This revives your pokémon after it has fainted", true, 30);
+    item[4] = Items("Great Ball", "This lets you have a higher chance at catching a pokémon!", false);
 }
-Inventory::Inventory(vector<Items> item, int quantity){
+Inventory::Inventory(vector<Items> item){
     //Not sure what to do here yet.
+    Inventory();
 }
 
 void Inventory::setItem(const vector<Items> &itemName){
