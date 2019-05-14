1623595cf8356f9b06165464f3ba444552a9624f
licup <41974174+licup@users.noreply.github.com>
2019-05-14 13:41:36 -0700

    Update Inventory.cpp

--- implementation/Inventory.cpp
+++ implementation/Inventory.cpp
@@ -6,17 +6,11 @@
 #include <string>
 
 Inventory::Inventory() {
-    item[0] = Items("Pokéball", "This lets you have a chance at catching a pokémon!", false);
-    item[1] = Items("Potion", "This heals your pokémon by 20hp", true, 20);
-    item[2] = Items("Great Potion", "This heals your pokémon by 40hp", true, 40);
-    item[3] = Items("Revive", "This revives your pokémon after it has fainted", true, 30);
-    item[4] = Items("Great Ball", "This lets you have a higher chance at catching a pokémon!", false);
+    item[0] = Items("Super Potion", "This heals your pokémon by 20hp", true, 50);
+    item[1] = Items("Hyper Potion", "This heals your pokémon by 40hp", true, 200);
+    item[2] = Items("Revive", "This revives your pokémon after it has fainted", true, 30);
 }
 Inventory::Inventory(vector<Items> item){
     //Not sure what to do here yet.
     Inventory();
-}
-
-void Inventory::setItem(const vector<Items> &itemName){
-    item = itemName;
 }
