5ad8c4b74f3d7231872f8abd8ccd576fc6c244eb
licup <41974174+licup@users.noreply.github.com>
2019-05-15 12:25:01 -0700

    Changed functionality

--- main.cpp
+++ main.cpp
@@ -2,19 +2,24 @@
 //  main.cpp
 //  PokemonGame
 //
-//  Created by Jr on 4/18/19.
-//  Copyright © 2019 Edgar Licup. All rights reserved.
-//
 #include "Items.hpp"
 #include "Inventory.hpp"
 #include "Pokemon.hpp"
 #include <iostream>
 
+void battle(Pokemon p, Pokemon p2, Inventory i);
+
 int main() {
     Inventory i;
-    Pokemon p("Pikachoo", 230, 100, 215), p1("Ricardo", 999, 100, 999);
+    Pokemon p("Pikachoo", 230, 100, 215), p1("Ricardo", 999, 100, 999),p3("Mukbang",320,78, 100);
+    battle(p1, p3,i);
+    return 0;
+}
+
+void battle(Pokemon p, Pokemon p1, Inventory i){
     char move; int choice;
     //Battle Sequence - Can change this to a function for diff pokémon
+    
     cout << "You have encountered " << p1.getName() << "!" << endl;
     while(p1.getHealth() > 0 && p.getHealth() > 0){
         cout << "A - Attack\tI - Item\nR - Run\n";
@@ -22,7 +27,7 @@ int main() {
         switch(tolower(move)){
             case 'a':
                 cout << p.getName() << " attacks!" << endl
-                << "Deals " << p.getAtk() << "damage." << endl;
+                << "Deals " << p.getAtk() << " damage." << endl;
                 p1.lowerHP(p);
                 cout << p1.getName() << "'s HP: " << p1.getHealth() << endl;
                 if(p1.getHealth() <= 0){
@@ -30,7 +35,7 @@ int main() {
                     break;
                 }
                 cout << p1.getName() << " attacks!" << endl
-                << "Deals " << p1.getAtk() << "damage." << endl;
+                << "Deals " << p1.getAtk() << " damage." << endl;
                 p.lowerHP(p1);
                 if(p.getHealth() <= 0){
                     cout << p.getName() << " fainted." << endl;
@@ -42,14 +47,14 @@ int main() {
                 }
                 break;
             case 'i':
-                cout << "Choose what item:\n0 - Great Potion(50HP) \t1 - Hyper Potion(200HP)\n";
+                cout << "Choose what item:\n0 - Potion(20HP) 1 - Great Potion(50HP) \t2 - Hyper Potion(200HP)\n";
                 cin >> choice;
                 switch(choice){
                     case 0:
                         p.higherHP(i, choice);
-                        cout << p.getName() << "HP: " << p.getHealth() << endl;
+                        cout << p.getName() << " HP: " << p.getHealth() << endl;
                         cout << p1.getName() << " attacks!" << endl
-                        << "Deals " << p1.getAtk() << "damage." << endl;
+                        << "Deals " << p1.getAtk() << " damage." << endl;
                         p.lowerHP(p1);
                         if(p.getHealth() <= 0){
                             cout << p.getName() << " fainted." << endl;
@@ -62,9 +67,24 @@ int main() {
                         break;
                     case 1:
                         p.higherHP(i, choice);
-                        cout << p.getName() << "HP: " << p.getHealth() << endl;
+                        cout << p.getName() << " HP: " << p.getHealth() << endl;
                         cout << p1.getName() << " attacks!" << endl
-                        << "Deals " << p1.getAtk() << "damage." << endl;
+                        << "Deals " << p1.getAtk() << " damage." << endl;
+                        p.lowerHP(p1);
+                        if(p.getHealth() <= 0){
+                            cout << p.getName() << " fainted." << endl;
+                            break;
+                        }
+                        else{
+                            cout << p.getName() << "'s HP: " << p.getHealth() << endl;
+                            break;
+                        }
+                        break;
+                    case 2:
+                        p.higherHP(i, choice);
+                        cout << p.getName() << " HP: " << p.getHealth() << endl;
+                        cout << p1.getName() << " attacks!" << endl
+                        << "Deals " << p1.getAtk() << " damage." << endl;
                         p.lowerHP(p1);
                         if(p.getHealth() <= 0){
                             cout << p.getName() << " fainted." << endl;
@@ -94,5 +114,4 @@ int main() {
                 break;
         }
     }
-    return 0;
 }
